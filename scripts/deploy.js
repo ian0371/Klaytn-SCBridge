27db202e1ae42256e9e463659e3e6c774debc5a9
Chihyun Song <ian0371@gmail.com>
2022-03-11 15:06:01 +0900

    EN works

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -15,12 +15,52 @@ async function main() {
 
   // We get the contract to deploy
   const ENbridge = await hre.ethers.getContractFactory("Bridge");
-  const enbridge = await ENbridge.deploy(true);
+  const enbridge = await ENbridge.deploy(false);
   await enbridge.deployed();
+
+  const SCbridge = await hre.ethers.getContractFactory("Bridge");
+  const scbridge = await SCbridge.deploy(false);
+  await scbridge.deployed();
 
   const ENtoken = await hre.ethers.getContractFactory("ServiceChainToken");
   const entoken = await ENtoken.deploy(enbridge.address);
-  await entoken.deployed();
+  await entoken.deployed(enbridge.address);
+
+  const SCtoken = await hre.ethers.getContractFactory("ServiceChainToken");
+  const sctoken = await SCtoken.deploy(enbridge.address);
+  await sctoken.deployed(scbridge.address);
+
+  await entoken.addMinter(enbridge.address);
+  await sctoken.addMinter(scbridge.address);
+
+  const signers = await hre.ethers.getSigners();
+  const enop = signers[1].address;
+  const scop = signers[2].address;
+  console.log('EN operator:', enop);
+  console.log('SCN operator:', scop);
+
+  await enbridge.registerOperator(enop);
+  await scbridge.registerOperator(scop);
+
+  await enbridge.registerToken(entoken.address, sctoken.address);
+  await scbridge.registerToken(sctoken.address, entoken.address);
+
+  await enbridge.transferOwnership(enop);
+  await scbridge.transferOwnership(scop);
+
+  console.log(`subbridge.registerBridge("${scbridge.address}", "${enbridge.address}")`)
+  console.log(`subbridge.subscribeBridge("${scbridge.address}", "${enbridge.address}")`)
+  console.log(`subbridge.registerToken("${scbridge.address}", "${enbridge.address}", "${sctoken.address}", "${entoken.address}")`)
+
+  const alice = '0xc40b6909eb7085590e1c26cb3becc25368e249e9';
+
+  // request to the token contract
+  await entoken.requestValueTransfer(100, alice, 0, []);
+
+  // direct request to the bridge
+  await entoken.approve(enbridge.address, 100);
+  await enbridge.requestERC20Transfer(entoken.address, alice, 100, 0, []);
+  console.log(await entoken.balanceOf(enbridge.address));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
